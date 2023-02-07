149c377e5058034e04e2ad5c99ce47e85f19e341
zed-chi <zed-chi@mail.ru>
2023-02-08 00:46:19 +0300

    added service and use case

--- api/weather/views.py
+++ api/weather/views.py
@@ -1,8 +1,15 @@
+from typing import Optional
+
 import fastapi
 
+from api.weather.models import Location
+from services import wttr_in
 
 weather_router = fastapi.APIRouter(prefix="/weather")
 
-@weather_router.get("")
-def weather(city, units):
-    return fastapi.responses.RedirectResponse(url="/static/icon.ico")
+
+@weather_router.get("/")
+def weather(loc: Location = fastapi.Depends(), units: Optional[str] = None):
+    report = wttr_in.get_report_png(loc.city, units)
+    body = f"<pre>{report}</pre>"
+    return fastapi.responses.HTMLResponse(content=body)
---
+++ services/wttr_in.py
@@ -0,0 +1,16 @@
+import httpx
+import requests
+
+from api.weather.models import Location, Units
+
+API_URL = "https://wttr.in"
+
+
+def get_report_png(city: str, units: str):
+    """Queries wttr.in sesrvice and return png file"""
+    url = f"{API_URL}/{city}?{units}=&T="
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.text
+
+    return data
