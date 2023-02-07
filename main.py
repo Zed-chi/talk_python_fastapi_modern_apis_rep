0e618da425177534b23528bc2c0556bdf38ee256
zed-chi <zed-chi@mail.ru>
2023-02-07 20:13:12 +0300

    start route added

--- main.py
+++ main.py
@@ -20,7 +20,16 @@ def calculate(x:int=1, y:int=1, z:Optional[int]=None):
 
 @api.get("/")
 def index():
-    return "<a>qwe</a>"
+    body = """
+    <h1>Hello, this is a dump page</h1>
+    <a href='/api/calculate'>Test route with empty params</a>
+    <hr>
+    <a href='/api/calculate?x=1&y=2&z=3'>Test route with 1, 2, 3</a>
+    <hr>
+    <a href='/api/calculate?x=1&y=2'>Test route with 1, 2</a>
+    <hr>
+    """
+    return fastapi.responses.HTMLResponse(content=body) 
 
 
 if __name__ == "__main__":
