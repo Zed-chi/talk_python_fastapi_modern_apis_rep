9e2e66117d870c48f3a5f49be2f5b62b5a328614
zed-chi <zed-chi@mail.ru>
2023-02-08 00:45:18 +0300

    lint

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import fastapi
 import uvicorn
-from environs import Env 
-from starlette.templating import Jinja2Templates
+from environs import Env
 from fastapi.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
 from api.router import api_router
 
 env = Env()
@@ -12,16 +13,16 @@ env.read_env()
 api = fastapi.FastAPI()
 template_manager = Jinja2Templates(env.str("HTML_DIR", "templates"))
 api.mount(
-    "/static", StaticFiles(directory=env.str("ASSETS_DIR")), 
-    name="static"
+    "/static", StaticFiles(directory=env.str("ASSETS_DIR")), name="static"
 )
 api.include_router(api_router)
 
+
 @api.get("/")
-def index(request:fastapi.Request):
+def index(request: fastapi.Request):
     return template_manager.TemplateResponse(
-        "./index.html", context={"request":request}
-)
+        "./index.html", context={"request": request}
+    )
 
 
 @api.get("/favicon.ico")
