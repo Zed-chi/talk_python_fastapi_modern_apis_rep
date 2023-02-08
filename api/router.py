bab1adc0056c688f264ceac07a47076b50342f07
zed-chi <zed-chi@mail.ru>
2023-02-08 17:26:24 +0300

    added report router

--- api/router.py
+++ api/router.py
@@ -1,6 +1,8 @@
 import fastapi
 
-from api.weather import views
+from api.reports.views import reports_router
+from api.weather.views import weather_router
 
 api_router = fastapi.APIRouter(prefix="/api")
-api_router.include_router(views.weather_router)
+api_router.include_router(weather_router)
+api_router.include_router(reports_router)
