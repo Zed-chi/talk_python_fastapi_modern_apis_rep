8b582df25d6663d619acd30c668b0d2440eb1ab3
zed-chi <zed-chi@mail.ru>
2023-02-08 17:28:02 +0300

    added try/except block

--- api/weather/views.py
+++ api/weather/views.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
 import fastapi
+from httpx._exceptions import HTTPStatusError
+
 from api.weather.models import Location
 from services import wttr_in
 
@@ -8,7 +10,18 @@ weather_router = fastapi.APIRouter(prefix="/weather")
 
 
 @weather_router.get("/")
-async def weather(loc: Location = fastapi.Depends(), units: Optional[str] = None):
-    report = await wttr_in.async_get_report_png(loc.city, units)
-    body = f"<a href='/'>Back to main page</a><hr><pre>{report}</pre>"
-    return fastapi.responses.HTMLResponse(content=body)
+async def weather(
+    loc: Location = fastapi.Depends(), units: Optional[str] = None
+):
+    try:
+        report = await wttr_in.async_get_report_png(loc.city, units)
+        body = f"<a href='/'>Back to main page</a><hr><pre>{report}</pre>"
+        return fastapi.responses.HTMLResponse(content=body)
+    except HTTPStatusError as e:
+        body = f"""
+        <h1> Some error </h1>
+        <pre>
+        {e}
+        </pre>
+        """
+        return fastapi.responses.HTMLResponse(content=body)
